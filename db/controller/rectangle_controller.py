7dbcbfaf25a76646b6e1742eb74b7d92ff58f900
Ayumu-Nono <ayumu.bete@gmail.com>
2021-08-27 10:02:27 +0900

    fix: not use intersect1d

--- db/controller/rectangle_controller.py
+++ db/controller/rectangle_controller.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import numpy as np
+from numpy.lib.arraysetops import isin
 
 from model.rectangle import Rectangle
 from db.interactor.rectangle_interactor import RectangleInteractor
@@ -13,18 +14,16 @@ class RectangleController:
     def push_with_arr(self, ps: np.ndarray) -> bool:
         assert isinstance(ps, np.ndarray)
         assert ps.shape == (4, 2)
-        i_lefts = np.argmin(ps[:, 0])
-        i_rights = np.argmax(ps[:, 0])
-        i_belows = np.argmin(ps[:, 1])
-        i_uppers = np.argmax(ps[:, 1])
-        i_upper_left = np.intersect1d(i_lefts, i_uppers)[0]
-        i_upper_right = np.intersect1d(i_rights, i_uppers)[0]
-        i_below_left = np.intersect1d(i_lefts, i_belows)[0]
-        # i_below_right = np.intersect1d(i_rights, i_belows)[0]
-        x0, y0 = ps[i_below_left]
-        width = np.abs(ps[i_upper_right] - ps[i_upper_left])
-        height = np.abs(ps[i_upper_left] - ps[i_below_left])
-        rectangle = Rectangle(xy=(x0, y0), width=width, height=height)
+        xmin = np.min(ps[:, 0])
+        xmax = np.max(ps[:, 0])
+        ymin = np.min(ps[:, 1])
+        ymax = np.max(ps[:, 1])
+        assert xmin < xmax
+        assert ymin < ymax
+        x0, y0 = xmin, ymin
+        dx = float(xmax - xmin)
+        dy = float(ymax - ymin)
+        rectangle = Rectangle(xy=(x0, y0), width=dx, height=dy)
         is_success: bool = self.__rectangle_interactor.push(rectangle)
         return is_success
     
