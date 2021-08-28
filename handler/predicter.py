c0a84edcb495a451487584cf3848572b9bbd81d8
Ayumu-Nono <ayumu.bete@gmail.com>
2021-08-28 21:51:04 +0900

    make: predicter

---
+++ handler/predicter.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from db.controller.status_controller import StatusController
+from db.controller.command_controller import CommandController
+
+
+class Predicter:
+    def __init__(
+        self,
+        pred_status_controller: StatusController,
+        smtd_status_controller: StatusController,
+        command_controller: CommandController
+    ) -> None:
+        self.__pred_status_controller: StatusController = pred_status_controller
+        self.__smtd_status_controller: StatusController = smtd_status_controller
+        self.__command_controller: CommandController = command_controller
+
+    def set_initial_status(self, position: np.ndarray, angle: float) -> bool:
+        assert self.__pred_status_controller.get_latest_tstep_as_int() == 0
+        is_success = self.__pred_status_controller.push_with_arr(position, angle)
+        return is_success
+
+    def predict(self, dt: float) -> bool:
+        old_positon = self.__smtd_status_controller.get_latest_position_as_arr()
+        old_angle = self.__smtd_status_controller.get_latest_angle_as_float()
+        now_velocity = self.__command_controller.get_latest_velocity_as_float()
+        now_angular_v = self.__command_controller.get_latest_angular_velocity_as_float()
+        dx = now_velocity * np.cos(old_angle) * dt
+        dy = now_velocity * np.sin(old_angle) * dt
+        d_theta = now_angular_v * dt
+        now_position = old_positon + np.array([dx, dy])
+        now_angle = old_angle + d_theta
+        is_success = self.__pred_status_controller.push_with_arr(now_position, now_angle)
+        return is_success
+
+        
+
+        
+
+    
