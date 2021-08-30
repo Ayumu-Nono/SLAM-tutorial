4363e9459e49ad7cad5038bca38151f21accee56
Ayumu-Nono <ayumu.bete@gmail.com>
2021-08-30 09:31:24 +0900

    n=50000

--- params/smoother_params.py
+++ params/smoother_params.py
@@ -2,10 +2,10 @@ from .hyper_params import mesh_digit, mesh_xs, mesh_ys, n_particle
 
 # Mesh
 mesh_digit, mesh_xs, mesh_ys = mesh_digit, mesh_xs, mesh_ys
-passing_rate = 0.6
+passing_rate = 0.8
 max_distance = 0.5
 position_vs_angle_cost_ratio = 0.05
-mesh_vs_status_cost_ratio = 0.5
+mesh_vs_status_cost_ratio = 1.0
 
 # Particle
 n_particle = n_particle
