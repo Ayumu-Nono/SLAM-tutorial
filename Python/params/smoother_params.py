2501b54363cfb43a3f9c62b58249df82a40739b2
Ayumu-Nono <ayumu.bete@gmail.com>
2021-08-30 12:18:24 +0900

    change dir

--- Python/params/smoother_params.py
+++ Python/params/smoother_params.py
@@ -2,7 +2,7 @@ from .hyper_params import mesh_digit, mesh_xs, mesh_ys, n_particle
 
 # Mesh
 mesh_digit, mesh_xs, mesh_ys = mesh_digit, mesh_xs, mesh_ys
-passing_rate = 0.5
+passing_rate = 0.9
 max_distance = 0.5
 position_vs_angle_cost_ratio = 0.05
 mesh_vs_status_cost_ratio = 1.0
